f84152766622f6182693249ddf9b19f595f75ed3
Raúl Eduardo González Argote <rafex@MacBook-Pro-de-Raul.local>
2019-04-23 11:01:50 -0500

    ejercicios

--- Ejercicios/Principal.java
+++ Ejercicios/Principal.java
@@ -1,39 +1,53 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.lang.Exception;
 
 public class Principal {
 
   public static void main (String[] args) {
 
-    Scanner entrada = new Scanner(System.in);
-
-    System.out.println("Inserta el primer número");
+     Scanner entrada;
 
     int numero1 = 0;
     int numero2 = 0;
 
-    try{
-        numero1=entrada.nextInt();
-        if(numero1 <= 0){
-            System.out.println("El número es inferior o igual a 0");
-            System.exit(0);
-        }
-    } catch(InputMismatchException e){
-        System.out.println("Este no es un número válido");
-        System.exit(0);
-    }
+    boolean validador1=true;
+    boolean validador2=true;
 
-    System.out.println("Inserta el segundo número");
-    try{
-        numero2=entrada.nextInt();
-        if(numero2 <= 0){
-            System.out.println("El número es inferior o igual a 0");
-            System.exit(0);
+    do{
+        try{
+            System.out.println("Inserta el primer número");
+            entrada = new Scanner(System.in);
+            numero1=entrada.nextInt();
+            if(numero1 <= 0){
+                throw new Exception("El número es inferior o igual a 0 del primer número");
+            }
+            validador1=false;
+        } catch(InputMismatchException e){
+            System.out.println("Este no es un número válido (primer número)");
+        } catch(Exception e){
+            System.out.println("El número es inferior o igual a 0 del primer número");
         }
-    } catch(InputMismatchException e){
-        System.out.println("Este no es un número válido");
-        System.exit(0);
-    }
+
+    }while(validador1);
+
+    do{
+        System.out.println("Inserta el segundo número");
+        try{
+            entrada = new Scanner(System.in);
+            numero2=entrada.nextInt();
+            if(numero2 <= 0){
+              throw new Exception("El número es inferior o igual a 0 del primer número");
+            }
+
+            validador2=false;
+        } catch(InputMismatchException e){
+            System.out.println("Este no es un número válido (segundo número)");
+        }catch(Exception e){
+            System.out.println("El número es inferior o igual a 0 del segundo número");
+        }
+
+    }while(validador2);
 
 
 
