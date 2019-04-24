16b61c97904304ad36c63f6fc8aecd66c8df3eb8
carlos <carlos_hdz.fabian@yahoo.com.mx>
2019-04-24 10:05:29 -0500

    mi primer ejercicio de arreglos

---
+++ Arreglos/src/mx/rafex/cursos/introduccion/arreglos/carlos/Ejercicio1.java
@@ -0,0 +1,32 @@
+package mx.rafex.cursos.introduccion.arreglos.carlos;
+
+import java.util.Scanner;
+public class Ejercicio1 {
+	public static void main(String [] args) {
+		
+		int suma = 0;
+	
+		
+		try (final Scanner entrada = new Scanner(System.in)) {
+
+            System.out.println("Insertar la dimensión del arreglo");
+            final int dimensionArreglo = entrada.nextInt();
+
+            final int[] arregloNumeros = new int[dimensionArreglo];
+
+            for (int indice = 0; indice < arregloNumeros.length; indice++) {
+                System.out.println("Insertar número en la posición [" + indice + "]");
+                arregloNumeros[indice] = entrada.nextInt();
+            }
+
+            System.out.println("Mostrando valores del arreglo");
+            for (int indice = 0; indice < arregloNumeros.length; indice++)
+                System.out.println("Valor del arreglo 'arregloNumeros' en la posición [" + indice + "] : "
+                        + arregloNumeros[indice]);
+            
+            
+            System.out.println("la suma del arrego es " + suma  );
+        }
+	}
+
+}
