75f0bf082280a1dd358bf7b4d7efffa15e145342
Israel Lopez <israelisaaclopezsanchez@hotmail.com>
2019-04-24 10:48:22 -0500

    mi primer ejercicio con java suma de matrices bidimensionales

--- Arreglos/src/mx/rafex/cursos/introduccion/arreglos/israel/ejercicio1.java
+++ Arreglos/src/mx/rafex/cursos/introduccion/arreglos/israel/ejercicio1.java
@@ -1,5 +1,13 @@
 package mx.rafex.cursos.introduccion.arreglos.israel;
 
 public class ejercicio1 {
-
+public static void main (String[] args) {
+	final int [] filaNumero = new int[5];
+	final int [] columnaNumero = new int [5];
+	int sumaColumna = 0;
+	
+	
+	
+	
+}
 }
