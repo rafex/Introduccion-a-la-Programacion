c51e57d1fe3960699f1b9b5383814749e369367d
Raúl González <rafex@rafex.mx>
2019-04-24 01:35:12 -0500

    para manejo de nmeros

---
+++ Numeros/src/mx/rafex/cursos/introduccion/numeros/Enteros.java
@@ -0,0 +1,12 @@
+package mx.rafex.cursos.introduccion.numeros;
+
+public class Enteros {
+
+    public static void main(String[] args) {
+
+        System.out.println(Integer.MAX_VALUE); // 2147483647
+        System.out.println(Integer.MAX_VALUE + 1); // -2147483648
+
+    }
+
+}
