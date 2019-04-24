8461d6064058cbd371695946de1592167e9ec44b
Sergio Ramirez <sergio.ramirez.mtz@gmail.com>
2019-04-24 10:07:29 -0500

    Hola

--- Arreglos/src/mx/rafex/cursos/introduccion/arreglos/sergio/Ejercicio1.java
+++ Arreglos/src/mx/rafex/cursos/introduccion/arreglos/sergio/Ejercicio1.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Ejercicio1 {
 	public static void main(String[] args) {
 		
+		int suma = 0;
+		
 		final Scanner entrada = new Scanner(System.in);
 		
 		System.out.println("Introduce un numero");
@@ -13,10 +15,10 @@ public class Ejercicio1 {
 		
 		for(int i = 0; i < arregloNumeros.length; i++) {
 			System.out.println(i);
-			
+			suma = suma + i;
 		}
-
 		
+		System.out.println("La suma de los numeros en el arreglo es: " + suma);
 		
 		
 	}
