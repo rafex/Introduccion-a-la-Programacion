f2c53ddbbdfd280a690d0f9dd43c15f77a4782e0
Israel Lopez <israelisaaclopezsanchez@hotmail.com>
2019-04-30 10:09:31 -0500

    se corrigieron errores en el constructor

--- Tareas/src/mx/rafex/cursos/introduccion/tareas/primera/israel/ConstructorSuma.java
+++ Tareas/src/mx/rafex/cursos/introduccion/tareas/primera/israel/ConstructorSuma.java
@@ -7,16 +7,11 @@ public class ConstructorSuma {
 	public Integer resultado = digito1 + digito2;
 	
 	
-	public ConstructorSuma (Integer digito1, Integer digito2, Integer resultado) {
+	public ConstructorSuma (Integer digito1, Integer digito2) {
 		this.digito1 = digito1;
 		this.digito2 = digito2;
-		this.resultado = resultado;
-		resultado = digito1+digito2;
-		System.out.println("El resultado de la suma es" + resultado);
-		}
 		
-		
-			
-		
+		System.out.println("El resultado de la suma es" +" "+ resultado);
+		}			
 }
 
