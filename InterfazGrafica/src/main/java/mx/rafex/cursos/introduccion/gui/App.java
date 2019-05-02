bcf156db4f6392580c63eedd1c0d8660d07eb9bb
Raúl González <rafex@rafex.mx>
2019-05-01 23:29:53 -0500

    examen

---
+++ InterfazGrafica/src/main/java/mx/rafex/cursos/introduccion/gui/App.java
@@ -0,0 +1,14 @@
+package mx.rafex.cursos.introduccion.gui;
+
+public class App {
+
+    public static void main(final String[] args) {
+
+//        java.awt.EventQueue.invokeLater(() -> new MyFrame().setVisible(true));
+
+        final MyFrame myFrame = new MyFrame("Mi App");
+        myFrame.setVisible(true);
+
+    }
+
+}
