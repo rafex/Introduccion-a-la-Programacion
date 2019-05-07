815005dcc2223dcc20976cad671345cd82fa1981
Raúl González <rafex.dev@gmail.com>
2019-05-07 09:30:58 -0500

    polimorfismo

--- Clases/src/mx/rafex/cursos/introduccion/clases/polimorfismo/Cuadrado.java
+++ Clases/src/mx/rafex/cursos/introduccion/clases/polimorfismo/Cuadrado.java
@@ -1,11 +1,11 @@
 package mx.rafex.cursos.introduccion.clases.polimorfismo;
 
-public class Cuadrado extends Figura {
+public class Cuadrado extends Figura2D {
 
     private final int lado;
 
-    public Cuadrado(final int lado, final String color) {
-        super(color);
+    public Cuadrado(final int lado, final String nombre, final String color) {
+        super(nombre, color);
         this.lado = lado;
     }
 
